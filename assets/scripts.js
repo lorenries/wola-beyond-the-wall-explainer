229edc4c8ece37909f4f43cb2ffa54f12e8ce43f
lorenries <lorenries@users.noreply.github.com>
2017-09-11 17:21:48 -0400

    initial commit

---
+++ assets/scripts.js
@@ -0,0 +1,52 @@
+document.addEventListener('DOMContentLoaded', function(e) {
+
+	feather.replace( {"height":"1em", "width": "1em"} );
+
+	new StickyState(document.querySelectorAll('.sticky'));
+
+	var disabledLink = document.querySelector('.disabled');
+	
+	if (disabledLink) {
+		disabledLink.addEventListener('click', function(e) { e.preventDefault(); });
+	}
+
+	var popup = document.querySelector('.js-popup');
+
+	var popupClose = document.querySelector('.js-close-popup');
+
+	function scrollFn() {
+
+		if(window.pageYOffset > 500) {
+
+			popup.classList.remove('dn');
+
+			return;
+
+		}
+	}
+
+
+	if (popup) {
+		document.addEventListener('scroll', scrollFn, false);
+	}
+
+	if (popupClose) {
+
+		popupClose.addEventListener('click', function(e) {
+
+			e.preventDefault();
+			popup.classList.add('dn');
+
+			// kill scroll listener
+			document.removeEventListener('scroll', scrollFn, false);
+
+
+		}, false);
+	}
+
+
+
+}, false);
+
+
+
